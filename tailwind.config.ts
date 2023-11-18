7acb36716af0b11789f38508d522c543d6c3a9b4
Tommy Chow <tommyxchow@gmail.com>
2023-11-17 22:02:29 -0500

    Refactor to ESM in `tailwind.config.ts`

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from 'tailwindcss';
 
-const config: Config = {
+export default {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -10,5 +10,4 @@ const config: Config = {
     extend: {},
   },
   plugins: [],
-};
-export default config;
+} satisfies Config;
