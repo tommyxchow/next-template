b27e49a3109811ab7ad827a097dc92693d4827b4
Tommy Chow <tommyxchow@gmail.com>
2023-11-18 15:04:08 -0500

    Install and set up `tailwindcss/forms` Resets default form styles for easier and consistent styling.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+import forms from '@tailwindcss/forms';
 import { type Config } from 'tailwindcss';
 
 export default {
@@ -13,5 +14,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [forms],
 } satisfies Config;
