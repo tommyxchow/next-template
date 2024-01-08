feb112a040d124d83b1f07d1662041dc61c4c150
Tommy Chow <tommyxchow@gmail.com>
2024-01-07 20:22:10 -0500

    Add `lib/constants.ts`

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
+import { githubRepoUrl } from '@/lib/constants';
+
 export default function Home() {
   return (
     <article>
       <h1>
         Welcome to{' '}
-        <a
-          className='underline'
-          href='https://github.com/tommyxchow/next-template'
-          target='_blank'
-        >
+        <a className='underline' href={githubRepoUrl} target='_blank'>
           next-template
         </a>
       </h1>
