1a7aaafacce1193001722b8a3a6037d980a09060
Tommy Chow <tommyxchow@gmail.com>
2025-05-10 14:45:04 -0700

    add `react-scan`

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,11 +1,20 @@
 'use client'
+// react-scan must be imported before react
+import { scan } from 'react-scan'
 
 import { ThemeProvider } from 'next-themes'
+import { useEffect } from 'react'
 
 interface ProvidersProps {
   children: React.ReactNode
 }
 
 export function Providers({ children }: ProvidersProps) {
+  useEffect(() => {
+    scan({
+      enabled: true,
+    })
+  }, [])
+
   return <ThemeProvider attribute='class'>{children}</ThemeProvider>
 }
